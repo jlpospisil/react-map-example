db351e2c4b554604c78b8bed9f15d259fb004228
jlpospisil <josh.pospisil@gmail.com>
2018-12-19 14:09:07 -0600

    Added router and top nav

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, NavLink, Redirect } from "react-router-dom";
 import '../scss/app.scss';
 import MapContainer from './MapContainer';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <MapContainer></MapContainer>
-      </div>
+      <Router>
+        <div className="App">
+          <nav className="AppNav">
+            <NavLink to="/basic">Basic</NavLink>
+            <NavLink to="/polygon">Polygon</NavLink>
+            <NavLink to="/heat-map">HeatMap</NavLink>
+          </nav>
+
+          <div className="AppContent">
+            <Route path="/basic" exact={true} component={MapContainer} />
+            <Route path="/polygon" exact={true} component={MapContainer} />
+            <Route path="/heat-map" exact={true} component={MapContainer} />
+            <Redirect to="/basic" />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
